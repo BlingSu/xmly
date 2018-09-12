f512353fff3921a38bcc8394885b0dd75d5f3869
angelasubi <21978301@qq.com>
2018-09-12 15:00:15 +0800

    update: cli

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
-const program = require('commander')
-const pkg = require(`./package.json`)
-
-program
-  .command('init')
-  .version(pkg.version)
-  .description(`创建html5结构目录...🐲`)
-  .action(options => {
-    console.log(options)
-  })
-  .parse(process.argv)
+require(`./lib/common/global`)
+require(`./lib/index`)
